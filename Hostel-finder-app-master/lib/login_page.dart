2d244d3c80f57e1d4b66d9565fb4ef7a524bdc52
GANDI-DILEEP <dileepgandi40@gmail.com>
2023-11-11 14:21:25 +0530

    commit dileep

--- Hostel-finder-app-master/lib/login_page.dart
+++ Hostel-finder-app-master/lib/login_page.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hostel_app/AuthProvider.dart';
 import 'package:hostel_app/home.dart';
+import 'package:hostel_app/signup.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,7 +17,6 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final CustomAuthProvider _authProvider = CustomAuthProvider();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
@@ -48,6 +50,30 @@ class _LoginScreenState extends State<LoginScreen> {
               child: const Text('Login'),
             ),
             const SizedBox(height: 16),
+            const SizedBox(height: 20), // Add spacing
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have an account? ",
+                    style: TextStyle(
+                      color: Colors.black,
+                    )),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => SignUpScreen(),
+                      ),
+                    );
+                  },
+                  child: const Text('Register',
+                      style: TextStyle(
+                        color: Colors.black,
+                      )),
+
+                ),
+              ],
+            ),
             ElevatedButton(
               onPressed: () {
                 signInWithGoogle(context);
@@ -61,26 +87,26 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Future<void> signInWithEmailPassword(BuildContext context) async {
-    String email = emailController.text;
     String password = passwordController.text;
 
     try {
-      UserCredential? userCredential =
-      await _authProvider.signInWithEmailPassword(email, password);
-
-      if (userCredential?.user != null) {
-        // Navigate to Professor Home Page or next screen
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const HomePage(),
-          ),
-        );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Sign-in failed. Please try again.')),
-        );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      String errorMessage = 'Sign-in failed. Please try again.';
+      if (e.code == 'user-not-found') {
+        errorMessage = 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        errorMessage = 'Wrong password provided for that user.';
       }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(errorMessage)),
+      );
     } catch (e) {
       print('Error signing in: $e');
       ScaffoldMessenger.of(context).showSnackBar(
@@ -88,6 +114,7 @@ class _LoginScreenState extends State<LoginScreen> {
       );
     }
   }
+
 
   Future<void> signInWithGoogle(BuildContext context) async {
     try {
